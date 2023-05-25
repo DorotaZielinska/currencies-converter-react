b85ee46d8d2c6b024ba995ca297310ac68193682
Dorota Zielinska <dorcius@protonmail.com>
2023-05-25 17:48:39 +0200

    update of raw version of currency converter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+       <form className="form ">
+        <fieldset className="form__fieldset">
+            <legend className="form__legend">Kalkulator walut</legend>
+            <p>
+                <label>
+                    <span className="form__labelText">Kwota w PLN*:</span>
+                    <input className="form__field" name="amountInPln" type="number" min="1" step="any"
+                        required />
+                </label>
+            </p>
+            <p>
+                <label>
+                    <span className="form__labelText">Przelicz na:</span>
+                    <select className="form__field" name="foreignCurrency">
+                        <option value="EUR">Euro</option>
+                        <option value="USD">Dolar amerykański</option>
+                        <option value="GBP">Funt brytyjski</option>
+                    </select>
+                </label>
+            </p>
+            <p>
+                <label>
+                    <span className="form__labelText">Kwota po przeliczeniu:</span>
+                    <input className="form__field" name="amountAfterConvert" type="number" disabled />
+                </label>
+            </p>
+        </fieldset>
+        <p>*Pole obowiązkowe</p>
+        <div>
+            <button className="form__button">Przelicz</button>
+        </div>
+        <div>
+            <button className="form__button" type="reset">Wyczyść</button>
+        </div>
+        <p className="form__paragraph">Kalkulator przelicza wartość dowolnie wybranych walut. Najnowsza tabela kursów
+            średnich NBP pochodzi z
+            dnia
+            2023-01-09.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    </form>
     </div>
   );
 }
