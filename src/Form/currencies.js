ca2cfffbff8c5f5e44dfed71d13af64299208b85
Dorota Zielinska <dorcius@protonmail.com>
2023-05-29 11:52:11 +0200

    added new file table  with objects of currencies

---
+++ src/Form/currencies.js
@@ -0,0 +1,18 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.69,
+    },
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.40,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.35,
+    },
+
+];
