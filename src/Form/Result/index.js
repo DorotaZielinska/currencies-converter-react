f4d1d687451ae1d3f4f0145fde029952c7e4ba60
Dorota Zielinska <dorcius@protonmail.com>
2023-05-29 11:53:44 +0200

    added new folder component of Result

---
+++ src/Form/Result/index.js
@@ -0,0 +1,9 @@
+export const Result = () => (
+    <p>
+         <label>
+        <span className="form__labelText">Kwota po przeliczeniu:</span>
+        <input className="form__field" name="amountAfterConvert" type="number" disabled />
+    </label>
+    </p>
+   
+)
