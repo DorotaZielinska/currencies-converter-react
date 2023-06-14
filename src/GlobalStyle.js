fc776ff750f476410d0843fb7827f61c4c7b713a
Dorota Zielinska <dorcius@protonmail.com>
2023-06-14 16:31:37 +0200

    added Global Style

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,21 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+import background from "./dollar-landscape.jpg"
+
+export const GlobalStyle = createGlobalStyle`
 html {
   box-sizing: border-box;
 }
-
 *, ::after,
- ::before {
+::before {
   box-sizing: inherit;
 }
-
 body {
   max-width: 700px;
   margin: 0 auto;
   padding: 0 20px;
   font-family: 'Inter', sans-serif;
   color: black;
-  background-image: url("./dollar-landscape.jpg");
+  background-image: url("${background}");
   background-size: cover;
   background-position: center;
-  background-repeat: no-repeat;
-  
+  background-repeat: no-repeat; 
 }
+`;
