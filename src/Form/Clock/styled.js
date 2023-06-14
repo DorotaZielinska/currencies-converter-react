2a6886d3dbf01448228139d749d565c95d783c40
Dorota Zielinska <dorcius@protonmail.com>
2023-06-14 14:58:55 +0200

    corrected styled component for Clock

--- src/Form/Clock/styled.js
+++ src/Form/Clock/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.paragraph`
+export const Wrapper = styled.div`
     text-align: right; 
     color: #6d6464;
     font-family: monospace;
